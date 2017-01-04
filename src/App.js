52ca4acf61796414d0a599432cfad78f3b3b3930
Andreas Reiterer <reiterer.a@gmail.com>
2017-01-04 16:29:10 +0100

    Connected the app to OpenWeatherMap API and fetch position with geolocation

--- src/App.js
+++ src/App.js
@@ -2,17 +2,48 @@ import React, { Component } from 'react';
 import './App.css';
 
 import WeatherData from './components/WeatherData';
-import { testData } from './sampleData';
+
+import { fetchWeatherInfoByGeolocation } from './apiCalls';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+
+    this.getWeatherInfoByLocation = this.getWeatherInfoByLocation.bind(this);
+  }
+
+  getWeatherInfoByLocation() {
+    // TODO: Error handling (fetch+geolocation)!
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        fetchWeatherInfoByGeolocation(position.coords.longitude, position.coords.latitude)
+          .then((data) => {
+            this.setState({ weatherInfo: data })
+          });
+      });
+    } else {
+      console.log('Geolocation is not supported by this browser.');
+    }
+  }
+
+  componentDidMount() {
+    this.getWeatherInfoByLocation();
+  }
+
   render() {
     return (
       <div className="App">
-        <WeatherData
-          cityName={testData.name}
-          temp={testData.main.temp}
-          minTemp={testData.main.temp_min}
-          maxTemp={testData.main.temp_max}/>
+        {this.state.weatherInfo ?
+          <WeatherData
+            cityName={this.state.weatherInfo.name}
+            temp={this.state.weatherInfo.main.temp}
+            minTemp={this.state.weatherInfo.main.temp_min}
+            maxTemp={this.state.weatherInfo.main.temp_max}/>
+        : 'no-data'}
+
       </div>
     );
   }
---
+++ src/apiCalls.js
@@ -0,0 +1,9 @@
+const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?appid=f57105642b469ddc58402f502ed12347';
+
+export function fetchWeatherInfoByGeolocation(long, lat) {
+  return fetch(baseUrl+'&lon='+long+'&lat='+lat+'&units=metric', {
+    method: 'get'
+  }).then((response) => {
+    return response.json()
+  });
+}
